721600392bd40f8cb17707efc0de23ce01ac89b4
Kangmin Park <l4stpr0gr4m@gmail.com>
2020-09-10 23:05:36 -0700

    Add intent

--- src/main.c
+++ src/main.c
@@ -4,33 +4,34 @@
 #define DELAY 30000
 
 int main(int argc, char *argv[]) {
- int x = 0, y = 0;
- int max_y = 0, max_x = 0;
- int next_x = 0;
- int direction = 1;
+	int x = 0, y = 0;
+	int max_y = 0, max_x = 0;
+	int next_x = 0;
+	int direction = 1;
 
- initscr();
- noecho();
- curs_set(FALSE);
+	initscr();
+	noecho();
+	curs_set(FALSE);
 
- // Global var `stdscr` is created by the call to `initscr()`
- getmaxyx(stdscr, max_y, max_x);
+	// Global var `stdscr` is created by the call to `initscr()`
+	getmaxyx(stdscr, max_y, max_x);
 
- while(1) {
- clear();
- mvprintw(y, x, "o");
- refresh();
+	while(1) {
+		clear();
+		mvprintw(y, x, "o");
+		refresh();
 
- usleep(DELAY);
+		usleep(DELAY);
 
- next_x = x + direction;
+		next_x = x + direction;
 
- if (next_x >= max_x || next_x < 0) {
- direction*= -1;
- } else {
- x+= direction;
- }
- }
+		if (next_x >= max_x || next_x < 0) {
+			direction*= -1;
+		} else {
+			x+= direction;
+		}
+	}
 
- endwin();
+	endwin();
+	return 0;
 }
